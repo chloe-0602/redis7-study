efb246632892f6a84e1514110ef18fcc0d2e505d
HerryWEu <yihong.wu@mhp.com>
2024-08-17 11:28:44 +0800

    feat: standalone

---
+++ src/main/java/com/chloe/Redis7Study7777.java
@@ -0,0 +1,15 @@
+package com.chloe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redis7Study7777
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Redis7Study7777.class,args);
+    }
+}
+
+
---
+++ src/main/java/com/chloe/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.chloe.controller;
+
+import com.chloe.service.OrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+
+@Controller
+@Api(tags = "订单接口")
+public class OrderController {
+    @Resource
+    private OrderService orderService;
+
+    @ApiOperation("新增订单")
+    @RequestMapping(value = "/order/add",method = RequestMethod.POST)
+    public void addOrder()
+    {
+        orderService.addOrder();
+    }
+
+
+    @ApiOperation("按orderId查订单信息")
+    @RequestMapping(value = "/order/{id}", method = RequestMethod.GET)
+    public String findUserById(@PathVariable Integer id)
+    {
+        return orderService.getOrder(id);
+    }
+}
