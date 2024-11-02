cd3a568a1874c9977056839f2d47d0b7d4bd7502
HerryWEu <yihong.wu@mhp.com>
2024-11-02 18:38:30 +0800

    feat:手写bloomfilter， 第一步 mybatis通用mapper4

--- src/main/java/com/chloe/Redis7Study7777.java
+++ src/main/java/com/chloe/Redis7Study7777.java
@@ -2,8 +2,10 @@ package com.chloe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.chloe.mapper")
 public class Redis7Study7777 {
     public static void main(String[] args) {
         SpringApplication.run(Redis7Study7777.class,args);
---
+++ src/test/java/com/chloe/MapperTest.java
@@ -0,0 +1,34 @@
+package com.chloe;
+
+import com.chloe.entity.Customer;
+import com.chloe.mapper.CustomerMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * ClassName: MapperTest
+ * Package: com.chloe
+ * Description:
+ *
+ * @Author Xu, Luqin
+ * @Create 2024/11/2 18:22
+ * @Version 1.0
+ */
+@Slf4j
+@SpringBootTest
+public class MapperTest {
+    @Autowired
+    private CustomerMapper customerMapper;
+    @Test
+    public void test1(){
+        Customer customer = new Customer();
+        customer.setAge(19);
+        customer.setCname("herry");
+        customer.setSex(Byte.valueOf("1"));
+        customer.setPhone("13262816311");
+        int rows = customerMapper.insertSelective(customer);
+        log.info("插入数据： {}", rows);
+    }
+}
