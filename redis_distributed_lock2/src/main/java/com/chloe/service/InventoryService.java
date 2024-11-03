b5e6b319f30aee23a9d11fd06b1b6829428bcf1f
HerryWEu <yihong.wu@mhp.com>
2024-11-03 17:26:55 +0800

    feat: 分布锁 修改为6.0版本，使用lua脚本实现，通过判断是否存在这个value的时候 后删除这个key的 原子性

--- redis_distributed_lock2/src/main/java/com/chloe/service/InventoryService.java
+++ redis_distributed_lock2/src/main/java/com/chloe/service/InventoryService.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -31,11 +33,65 @@ public class InventoryService {
     private static final String INVENTORY_KEY_01 = "inventory001";
 
     /**
-     * 5. 防止误删key -》 对应 【不乱抢】
+     * 6. lua保证原子性： finally块的判断 + del删除 不是原子性
      *
      * @return
      */
     public String sale() {
+        String message = "";
+        String redisLockKey = "chloeRedisLock";
+        String uuidValue = IdUtil.randomUUID() + ":" + Thread.currentThread().getId();
+
+
+        // 自旋
+        while (!stringRedisTemplate.opsForValue().setIfAbsent(redisLockKey, uuidValue, 30L, TimeUnit.SECONDS)) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+//        stringRedisTemplate.expire(redisLockKey,20L, TimeUnit.SECONDS);
+
+        try {
+            String inventoryNumberStr = stringRedisTemplate.opsForValue().get(INVENTORY_KEY_01);
+            Integer inventoryNum = inventoryNumberStr == null ? 0 : Integer.parseInt(inventoryNumberStr);
+            if (inventoryNum > 0) {
+                stringRedisTemplate.opsForValue().set(INVENTORY_KEY_01, String.valueOf(--inventoryNum));
+                message = "成功卖出一个商品，剩余：" + inventoryNum;
+                log.info(message);
+            } else {
+                message = "商品卖完了......";
+                log.info(message);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            /**
+             * 使用lua脚本实现，通过判断是否存在这个value的时候 后删除这个key的 原子性
+             * ！！注意，DefaultRedisScript需要加上返回的 Boolean.class 否则会报错
+             */
+            String luaScript = "if redis.call('get',KEYS[1]) == ARGV[1] then " +
+                    "return redis.call('del',KEYS[1]) " +
+                    "else " +
+                    "return 0 " +
+                    "end";
+            stringRedisTemplate.execute(new DefaultRedisScript<>(luaScript, Boolean.class),
+                    Arrays.asList(redisLockKey),
+                    uuidValue);
+        }
+
+        return message + "\t" + "服务端口号：" + port;
+    }
+
+
+    /**
+     * 5. 防止误删key -》 对应 【不乱抢】
+     *
+     * @return
+     */
+    public String saleV5() {
         String message = "";
         String redisLockKey = "chloeRedisLock";
         String uuidValue = IdUtil.randomUUID() + ":" + Thread.currentThread().getId();
